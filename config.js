bad65e89c8084aff2059eb5b700d2eacf2795813
Cameron Moreau <cameron.cm6@gmail.com>
2016-12-01 11:58:58 -0600

    added config

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mongo: 'mongodb://Cameron:camtheman@ds145365.mlab.com:45365/cse',
+  pusher: {
+    app: '273037',
+    key: '0caec063dbc96e698fc',
+    secret: 'da3e038a73620d26cad6',
+  },
+  mailgun: {
+    key: 'key-b6a3c726efdc3366866bc514fc7bf92e',
+    domain: 'cameronmoreau.org',
+    email: 'hello@cameronmoreau.org'
+  },
+  twilio: {
+    token: '2d400f4d6ef00d899b22174835f00429',
+    acc: 'ACd6126d732abac71fdeadd9948f89fc1f'
+  }
+}
